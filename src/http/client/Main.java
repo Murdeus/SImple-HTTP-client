b84317dbf556ce91f53f4c38f3c77fff09e4cc79
Константин Кожевников < >
2016-03-16 21:08:27 +0300

    ready to work

---
+++ src/http/client/Main.java
@@ -0,0 +1,9 @@
+package http.client;
+
+public class Main {
+	public static void main(String[] args) {
+		//new MainWindow("www.google.com", 80);
+		new MainWindow("192.168.137.13", 5225);
+		//new MainWindow("localhost", 8080);
+	}
+}
